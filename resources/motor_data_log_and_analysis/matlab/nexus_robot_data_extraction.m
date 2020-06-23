9e5a9a903e48318fd5bb09034c39abc2312e5111
RBinsonB <r-baran@hotmail.fr>
2020-06-23 14:58:19 +0200

    Added description to log and analysis Matlab scripts

--- resources/motor_data_log_and_analysis/matlab/nexus_robot_data_extraction.m
+++ resources/motor_data_log_and_analysis/matlab/nexus_robot_data_extraction.m
@@ -1,3 +1,18 @@
+%% ============================= Description ==============================
+% Run first the nexus_robot_pwm_logger.m script to get the PWM/velocity
+% data from the real robot.
+% 
+% This script generates a polynomial function for converting a velocity
+% command into a PWM command to apply to the motor. The polynomial 
+% coefficients for each motor (pUL, pUR, pLL, and pLR) obtained here need 
+% to be written to the nexus robot config file.
+%   
+%   pUL = UL motor (motor 4)
+%   pUR = UR motor (motor 1)
+%   pLL = LL motor (motor 3)
+%   pLR = LR motor (motor 2)
+%% ========================================================================
+
 vel_wheel = zeros(4,numel(log_vel));
 pwm_wheel = zeros(4,numel(log_vel));
 
@@ -17,14 +32,14 @@ end
 %b1 = log_pwm\transpose(vel_wheel(1,:));
 
 %Create polynomial fitting
-p1 = polyfit(transpose(vel_wheel(1,:)), log_pwm, 6);
-p2 = polyfit(transpose(vel_wheel(2,:)), log_pwm, 6);
-p3 = polyfit(transpose(vel_wheel(3,:)), log_pwm, 6);
-p4 = polyfit(transpose(vel_wheel(4,:)), log_pwm, 6);
-wheel_1_command_polynomial = polyval(p1, vel_wheel(1,:));
-wheel_2_command_polynomial = polyval(p2, vel_wheel(2,:));
-wheel_3_command_polynomial = polyval(p3, vel_wheel(3,:));
-wheel_4_command_polynomial = polyval(p4, vel_wheel(4,:));
+pUL = polyfit(transpose(vel_wheel(1,:)), log_pwm, 5);
+pUR = polyfit(transpose(vel_wheel(2,:)), log_pwm, 5);
+pLL = polyfit(transpose(vel_wheel(3,:)), log_pwm, 5);
+pLR = polyfit(transpose(vel_wheel(4,:)), log_pwm, 5);
+wheel_1_command_polynomial = polyval(pUL, vel_wheel(1,:));
+wheel_2_command_polynomial = polyval(pUR, vel_wheel(2,:));
+wheel_3_command_polynomial = polyval(pLL, vel_wheel(3,:));
+wheel_4_command_polynomial = polyval(pLR, vel_wheel(4,:));
 
 hold on;
 plot(vel_wheel(1,:), log_pwm, '-o');
