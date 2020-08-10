6932376a21aabd5f7744070488fa8fd56db4a3dd
RBinsonB <r-baran@hotmail.fr>
2020-08-10 12:32:03 +0200

    Add odometry node, now building (untested)

--- sml_nexus_robot/src/odometry_broadcaster.cpp
+++ sml_nexus_robot/src/odometry_broadcaster.cpp
@@ -5,192 +5,224 @@
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/Twist.h"
 #include "nav_msgs/Odometry.h"
+#include <tf/transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 class SmlNexusOdometryBroadcaster
 {
 public:
-	SmlNexusOdometryBroadcaster();
-	~SmlNexusOdometryBroadcaster();
+    SmlNexusOdometryBroadcaster();
+    ~SmlNexusOdometryBroadcaster();
 private:
-	void wheelVelCallback();
-	void publishOdometry();
-	void computeOdometry(const float ULWheelVel,
-						 const float URWheelVel,
-					     const float LLWheelVel,
-						 const float LRWheelVel,
-						 geometry_msgs::Pose prev_pose, 
-					 	 ros::Time time_interval);
+    void wheelVelCallback(const std_msgs::Float32MultiArray& msg);
+    void computeOdometry(nav_msgs::Odometry& odom,
+                         const float& ULWheelVel,
+                         const float& URWheelVel,
+                         const float& LLWheelVel, 
+                         const float& LRWheelVel, 
+                         ros::Duration& time_interval);
 
-	geometry_msgs::Twist computeVel(const float ULWheelVel,
-									const float URWheelVel,
-									const float LLWheelVel,
-									const float LRWheelVel);
+    geometry_msgs::Twist computeVel(const float& ULWheelVel,
+                                    const float& URWheelVel,
+                                    const float& LLWheelVel,
+                                    const float& LRWheelVel);
 
-	tf::Transform computeRelativeMotion();
+    nav_msgs::Odometry computeRelativeMotion(const float& ULWheelVel,
+                                             const float& URWheelVel,
+                                             const float& LLWheelVel,
+                                             const float& LRWheelVel,
+                                             const float& timeSeconds);
 
-	//ROS variables
-	//=============
-	void setSubAndPub();
-	std::string ns; //Parameters namespace
-	//Subscriber and publishers
-	ros::Subscriber vel_sub;
-	ros::Publisher odometry_pub;
-	static tf2_ros::TransformBroadcaster transform_broadcaster;
+    //ROS variables
+    //=============
+    void setSubAndPub(ros::NodeHandle& nh_);
+    std::string ns; //Parameters namespace
+    //Subscriber and publishers
+    ros::Subscriber feedback_sub;
+    ros::Publisher odom_pub;
+    tf2_ros::TransformBroadcaster transform_broadcaster;
 
-	//
-	float robot_wheelbase = 0.15; //robot wheelbase in meters
-	ros::Time last_received_data;
-	nav_msgs::Odometry odom_msg;
-	geometry_msgs::TransformStamped odom_transform;
-	geometry_msgs::Pose global_pose;
+    //
+    float robot_wheelbase = 0.15; //robot wheelbase in meters
+    ros::Time last_received_data;
+    nav_msgs::Odometry odom_msg;
+    geometry_msgs::TransformStamped odom_transform;
 
-}
+};
 
 //=====================
-// 	   constructor
+//        constructor
 //=====================
-SmlNexusOdometryBroadcaster(){
-	ros::NodeHandle nh;
-	ns = nh.getNamespace();
+SmlNexusOdometryBroadcaster::SmlNexusOdometryBroadcaster(){
+    ros::NodeHandle nh;
+    ns = nh.getNamespace();
 
-	ROS_INFO(ns+" Starting odometry broadcaster");
+    ROS_INFO_STREAM(ns << " Starting odometry broadcaster");
 
-	//Initialize the robot wheelbase radius used later on
-	const float robot_wheelbase_sq = robot_wheelbase * robot_wheelbase;
-	robot_wheelbase_radius = std::sqrt(2 * robot_wheelbase_sq);
-
-	//Setup ROS subscribers and publishers
-	setSubAndPub();
+    //Setup ROS subscribers and publishers
+    setSubAndPub(nh);
 }
 
+SmlNexusOdometryBroadcaster::~SmlNexusOdometryBroadcaster(){}
 
 //=======================================
 //   Setup ROS subscribers, publishers
 //        and pre-fill messages
 //=======================================
-void setSubAndPub(){
-	ROS_INFO(ns+" odometry broadcaster: setting up publishers and subscribers...");
+void SmlNexusOdometryBroadcaster::setSubAndPub(ros::NodeHandle& nh_){
+    ROS_INFO_STREAM(ns << " odometry broadcaster: setting up publishers and subscribers...");
 
-	//------------------------------------------
-	// Setup wheel velocity feedback subscriber
-	//------------------------------------------
-	feedback_sub = nh_.subscribe("wheel_velocity", 1000, &wheelVelCallback, this);
+    //------------------------------------------
+    // Setup wheel velocity feedback subscriber
+    //------------------------------------------
+    feedback_sub = nh_.subscribe("wheel_velocity", 1000, &SmlNexusOdometryBroadcaster::wheelVelCallback, this);
 
-	//--------------------------
-	// Setup odometry publisher
-	//--------------------------
-	cmd_pub = nh_.advertise<nav_msgs::Odometry>("odom", 1000);
+    //--------------------------
+    // Setup odometry publisher
+    //--------------------------
+    odom_pub = nh_.advertise<nav_msgs::Odometry>("odom", 1000);
 
-	//-----------------------------
-	// Initialize odometry message
-	//-----------------------------
-	odom_msg.header.frame_id = ns+"/odom";
-	odom_msg.child_frame_id = ns+"/base_link";
+    //-----------------------------
+    // Initialize odometry message
+    //-----------------------------
+    odom_msg.header.frame_id = ns+"/odom";
+    odom_msg.child_frame_id = ns+"/base_link";
 
-	//------------------------------
-	// Initialize transform message
-	//------------------------------
-	odom_transform.header.frame_id = ns+"/odom";
-	odom_transform.child_frame_id = ns+"/base_link";
-
-}
-
-
-wheelVelCallback(const std_msgs::Float32MultiArray& msg){
-	if (msg.data.size() != 4){
-		//Exit if msg is malformed
-	}
-	else{
-		ros::Time time_interval = ros::Time::now() - last_received_data;
-		odom_msg = computeOdometry(msg.data[0], msg.data[1], msg.data[2], msg.data[3], global_pose, time_interval);
-	}
-	
-
+    //------------------------------
+    // Initialize transform message
+    //------------------------------
+    odom_transform.header.frame_id = ns+"/odom";
+    odom_transform.child_frame_id = ns+"/base_link";
 
 }
 
-void computeOdometry(const float ULWheelVel,
-					 const float URWheelVel,
-					 const float LLWheelVel, 
-					 const float LRWheelVel, 
-					 geometry_msgs::Pose prev_pose, 
-					 ros::Time time_interval)
-{
-	odom_msg.twist.twist = computeVel(msg.data[0], msg.data[1], msg.data[2], msg.data[3]);
 
-	tf::Transform tmp;
-    tmp.setIdentity();
-
-
-    if (std::abs(odom_msg.twist.twist.angular.z) < 0.0001) {
-    //----------------
-    // Drive straight
-    //----------------
-      tmp.setOrigin(tf::Vector3(static_cast<double>(odom_msg.twist.twist.linear.x*timeSeconds), static_cast<double>(odom_msg.twist.twist.linear.y*timeSeconds), 0.0));
-    } else {
-    //---------------------
-    // Follow circular arc
-    //---------------------
-      const double distX = odom_msg.twist.twist.linear.x * timeSeconds;
-      const double distY = odom_msg.twist.twist.linear.y * timeSeconds;
-      const double distChange = std::sqrt(distX * distX + distY * distY);
-      const double angleDriveDirection = std::acos(distX / distChange);
-      const double angleChange = odom_msg.twist.twist.angular.z * timeSeconds;
-
-      const double arcRadius = distChange / angleChange;
-
-      tf::Vector3 endPos = tf::Vector3(std::sin(angleChange) * arcRadius,
-                                arcRadius - std::cos(angleChange) * arcRadius,
-                                0.0);
-
-      tmp.setOrigin(endPos.rotate(tf::Vector3(0.0, 0.0, 1.0), angleDriveDirection));
-
-      tmp.setRotation(tf::createQuaternionFromYaw(angleChange));
+void SmlNexusOdometryBroadcaster::wheelVelCallback(const std_msgs::Float32MultiArray& msg){
+    if (msg.data.size() != 4){
+        //Exit if msg is malformed
     }
+    else{
+        ros::Duration time_interval = ros::Time::now() - last_received_data;
+        computeOdometry(odom_msg, msg.data[0], msg.data[1], msg.data[2], msg.data[3], time_interval);
+
+        //Publish odometry
+        odom_pub.publish(odom_msg);
+
+        //Publish transform
+        odom_transform.transform.translation.x = odom_msg.pose.pose.position.x;
+        odom_transform.transform.translation.y = odom_msg.pose.pose.position.y;
+        odom_transform.transform.translation.z = odom_msg.pose.pose.position.z;
+        odom_transform.transform.rotation = odom_msg.pose.pose.orientation;
+        transform_broadcaster.sendTransform(odom_transform);
+    }
+    
+
 
 }
 
-//======================================
-//	Compute velocity in base link frame
-//======================================
-geometry_msgs::Twist computeVel(const float ULWheelVel, const float URWheelVel, const float LLWheelVel, const float LRWheelVel){
-	geometry_msgs::Twist relativeVel;
+void SmlNexusOdometryBroadcaster::computeOdometry(nav_msgs::Odometry& odom,
+                     const float& ULWheelVel,
+                     const float& URWheelVel,
+                     const float& LLWheelVel, 
+                     const float& LRWheelVel, 
+                     ros::Duration& time_interval)
+{   
+    const nav_msgs::Odometry relativeMotion = computeRelativeMotion(ULWheelVel, URWheelVel, LLWheelVel, LRWheelVel, time_interval.toSec());
+    tf2::Quaternion q_prev, q_rot, q_new;
 
-	relativeVel.linear.x = (ULWheelVel + URWheelVel + LLWheelVel + LRWheelVel) / 4;
-	relativeVel.linear.y = (- ULWheelVel + URWheelVel + LLWheelVel - LRWheelVel) / 4;
-	relativeVel.linear.z = 0.0;
+    odom.pose.pose.position.x = odom.pose.pose.position.x + relativeMotion.pose.pose.position.x;
+    odom.pose.pose.position.y = odom.pose.pose.position.y + relativeMotion.pose.pose.position.y;
+    odom.pose.pose.position.z = odom.pose.pose.position.z + relativeMotion.pose.pose.position.z;
 
-	relativeVel.angular.x = 0.0;
-	relativeVel.angular.y = 0.0;
-	relativeVel.angular.z = (- ULWheelVel + URWheelVel - LLWheelVel + LRWheelVel) / (8 * robot_wheelbase);
+    tf2::convert(odom.pose.pose.orientation, q_prev);
+    tf2::convert(relativeMotion.pose.pose.orientation, q_rot);
+    q_new = q_rot * q_prev;
+    q_new.normalize();
+    tf2::convert(q_new, odom.pose.pose.orientation);
 
-	return relativeVel;
+    odom.twist = relativeMotion.twist;
 }
 
-computeRelativeMotion()
+//======================================
+//    Compute velocity in base link frame
+//======================================
+geometry_msgs::Twist SmlNexusOdometryBroadcaster::computeVel(const float& ULWheelVel, const float& URWheelVel, const float& LLWheelVel, const float& LRWheelVel){
+    geometry_msgs::Twist relativeVel;
+
+    relativeVel.linear.x = (ULWheelVel + URWheelVel + LLWheelVel + LRWheelVel) / 4;
+    relativeVel.linear.y = (- ULWheelVel + URWheelVel + LLWheelVel - LRWheelVel) / 4;
+    relativeVel.linear.z = 0.0;
+
+    relativeVel.angular.x = 0.0;
+    relativeVel.angular.y = 0.0;
+    relativeVel.angular.z = (- ULWheelVel + URWheelVel - LLWheelVel + LRWheelVel) / (8 * robot_wheelbase);
+
+    return relativeVel;
+}
+
+nav_msgs::Odometry SmlNexusOdometryBroadcaster::computeRelativeMotion(const float& ULWheelVel, const float& URWheelVel, const float& LLWheelVel, const float& LRWheelVel, const float& timeSeconds){
+    nav_msgs::Odometry odom;
+    const geometry_msgs::Twist vel = computeVel(ULWheelVel, URWheelVel, LLWheelVel, LRWheelVel);
+    odom.twist.twist = vel;
+
+    if (std::abs(vel.angular.z) < 0.0001) {
+        //----------------
+        // Drive straight
+        //----------------
+        odom.pose.pose.position.x = static_cast<double>(vel.linear.x*timeSeconds);
+        odom.pose.pose.position.y = static_cast<double>(vel.linear.y*timeSeconds);
+        odom.pose.pose.position.z = 0.0;
+
+    } else {
+        //---------------------
+        // Follow circular arc
+        //---------------------
+        const double distX = vel.linear.x * timeSeconds;
+        const double distY = vel.linear.y * timeSeconds;
+        const double distChange = std::sqrt(distX * distX + distY * distY);
+        const double angleDriveDirection = std::acos(distX / distChange);
+        const double angleChange = vel.angular.z * timeSeconds;
+
+        const double arcRadius = distChange / angleChange;
+
+        tf::Vector3 endPos = tf::Vector3(std::sin(angleChange) * arcRadius,
+                                        arcRadius - std::cos(angleChange) * arcRadius,
+                                        0.0);
+
+        endPos = endPos.rotate(tf::Vector3(0.0, 0.0, 1.0), angleDriveDirection);
+
+        odom.pose.pose.position.x = endPos[0];
+        odom.pose.pose.position.y = endPos[1];
+        odom.pose.pose.position.z = 0.0;
+
+        tf::quaternionTFToMsg(tf::createQuaternionFromYaw(angleChange), odom.pose.pose.orientation);
+
+    }
+    return odom;
+}
 
 
-//=============================
-//			  Main
-//=============================
+//==============================
+//             Main
+//==============================
 int main(int argc, char** argv){
-	ros::init(argc, argv, "odometry_node");
-	
-	try{
-		SmlNexusOdometryBroadcaster odometry_broadcaster;
-		ros::spin();
-	}
-	//Error handling
-	catch (int error){
-		if (error == 1){
-			ROS_FATAL("Node can't initialize, failed to get parameters");
-		}
-		else{
-			ROS_FATAL("Node encountered an unexpected error");		
-		}
-		return 1;
-	}
-	return 0;
+    ros::init(argc, argv, "odometry_node");
+    
+    try{
+        SmlNexusOdometryBroadcaster odometry_broadcaster;
+        ros::spin();
+    }
+    //Error handling
+    catch (int error){
+        if (error == 1){
+            ROS_FATAL("Node can't initialize, failed to get parameters");
+        }
+        else{
+            ROS_FATAL("Node encountered an unexpected error");        
+        }
+        return 1;
+    }
+    return 0;
 }
